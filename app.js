486a153fabcd9ec752dae98c87bd7b6a42b8bb1c
faceyage <alperencingoz33@gmail.com>
2022-05-28 17:32:10 +0300

    Add Basic structure of grids and drawing

---
+++ app.js
@@ -0,0 +1,20 @@
+function createGrid(size) {
+    const container = document.querySelector("#grids");
+    for (let i = 0; i < size; i++) 
+    {
+        const line = document.createElement("div");
+        line.classList.add("line");
+        for (let j = 0; j < size; j++) 
+        {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            line.appendChild(grid);
+            grid.addEventListener("mouseenter", () => {
+                grid.style.backgroundColor = "black";
+            });
+        }
+        container.appendChild(line);
+    }
+}
+
+createGrid(16);
