60e016b6570bc81ec784dc9b39ab2dd33cb5284b
faceyage <alperencingoz33@gmail.com>
2022-05-29 17:53:19 +0300

    bug fixes

--- app.js
+++ app.js
@@ -16,30 +16,32 @@ function createGrids(size) {
             grid.classList.add("grid");
             grid.style.backgroundColor = bgColor;
             line.appendChild(grid);
-            grid.addEventListener("mouseover", () => {
-                if (currentMode === "colorMode") {
-                    grid.style.backgroundColor = penColor;
-                }
-                else if(currentMode === "eraserMode") {
-                    grid.style.backgroundColor = bgColor;
-                }
-                else if(currentMode === "rainbowMode") {
-                    grid.style.backgroundColor = createRandomColor();
-                    console.log("Not implemented yet");
-                }
-            });
+            grid.addEventListener("mouseover", (e) => changeGridColor(e));
         }
         container.appendChild(line);
     }
 };
 
-//returns a random color with rgb string like "rgb(0-255, 0-255, )"
+function changeGridColor(e) {
+    if (currentMode === "colorMode") {
+        e.target.style.backgroundColor = penColor;
+    }
+    else if(currentMode === "eraserMode") {
+        e.target.style.backgroundColor = bgColor;
+    }
+    else if(currentMode === "rainbowMode") {
+        e.target.style.backgroundColor = createRandomColor();
+        console.log("Not implemented yet");
+    }
+}
+
+//returns a random color with rgb array with 3 numbers
 function createRandomColor() {
     const RGB = [255,255,255];
     for (let i = 0; i < 3; i++) {
         RGB[i] = Math.floor(Math.random() * 256);
     }
-    return `rgb(${RGB[0]},${RGB[1]},${RGB[2]})`;
+    return `rgb(${RGB[0]}, ${RGB[1]}, ${RGB[2]})`;
 };
 // function clearGrids() {
 //     const container = document.querySelector("#grids");
@@ -47,28 +49,28 @@ function createRandomColor() {
 // }
 
 function addButtonListeners() {
-    //sizeBtn funtionality
+    //sizeBtn functionality
     const sizeBtn = document.querySelector("#sizeBtn");
     sizeBtn.addEventListener("click", () => {
         size = prompt("Size", 16);
         createGrids(size);
     });
 
-    //clearBtn functionallity
+    //clearBtn functionality
     const clearBtn = document.querySelector("#clearBtn");
     clearBtn.addEventListener("click", () => createGrids(size));
 
-    //colorBtn functionallity
+    //colorBtn functionality
     const colorBtn = document.querySelector("#colorBtn");
     colorBtn.addEventListener("click", () => {
         currentMode = "colorMode";
     });
-    //RainbowMode functionallity
+    //RainbowMode functionality
     const rainbowBtn = document.querySelector("#rainbowBtn");
     rainbowBtn.addEventListener("click", () => {
         currentMode = "rainbowMode";
     });
-    //eraserBtn functionallity
+    //eraserBtn functionality
     const eraserBtn = document.querySelector("#eraserBtn");
     eraserBtn.addEventListener("click", () => {
         currentMode = "eraserMode"; 
